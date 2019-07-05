68587ae35af35c58b52400708a8591291da62b05
EC2 Default User <ec2-user@ip-172-31-31-201.us-east-2.compute.internal>
2019-07-05 17:30:50 +0000

    secondcommit

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ def get_poem_data():
     input_word = request.form["input_word"]
     # next three lines are declaring that we will find a rhyme as the parameter and calling the API to look for that.
     line1, line2 = model.generate_lines(input_word)
-    return render_template('index.html', input_word = line1, output_word=line2)
+    return render_template('index.html', line1 = line1, line2=line2)
 
 @app.route('/')
 @app.route('/index')
